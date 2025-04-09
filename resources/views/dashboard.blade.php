aee9ee442b9c3b29d5d08798a658946f65a4bfee
Jonathan Valdivia <jonanicolasvb@gmail.com>
2025-04-09 17:37:44 -0400

    segundo del video

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,7 @@
 <x-layouts.app :title="__('Dashboard')">
+    
+    <p class="text-yellow-500">Hola mundo</p>
+    
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
